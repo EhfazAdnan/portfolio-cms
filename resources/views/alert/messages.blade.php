49ad81d2417c11b5d78d403f788c3546abdf120c
EhfazAdnan <ehfaz15-167@diu.edu.bd>
2021-12-23 23:53:55 -0800

    session error sucess message add

--- resources/views/alert/messages.blade.php
+++ resources/views/alert/messages.blade.php
@@ -18,3 +18,17 @@
             </div>
         @endif
 </div>
+
+@if (session('error'))
+    <div class="alert alert-danger alert-dismissible">
+        <button type="button" class="close" data-dismiss="alert">&times;</button>
+        <strong>Error!</strong> {{session('error')}}
+    </div>
+@endif
+
+@if (session('success'))
+    <div class="alert alert-success alert-dismissible">
+        <button type="button" class="close" data-dismiss="alert">&times;</button>
+        <strong>Error!</strong> {{session('success')}}
+    </div>
+@endif
