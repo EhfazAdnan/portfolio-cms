930ac8004aebcff44feedb79f63274a37eb01974
EhfazAdnan <ehfaz15-167@diu.edu.bd>
2021-12-24 06:44:51 -0800

    populate to frondend page

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -36,11 +36,11 @@
             </div>
         </nav>
         <!-- Masthead-->
-        <header class="masthead">
+        <header class="masthead" style="background-image: url(<?php echo $main->bc_img ?>)">
             <div class="container">
-                <div class="masthead-subheading">Welcome To Our Studio!</div>
-                <div class="masthead-heading text-uppercase">It's Nice To Meet You</div>
-                <a class="btn btn-primary btn-xl text-uppercase" href="#services">Tell Me More</a>
+                <div class="masthead-subheading">{{ $main->title }}</div>
+                <div class="masthead-heading text-uppercase">{{ $main->sub_title }}</div>
+                <a class="btn btn-primary btn-xl text-uppercase" href="{{url($main->resume)}}">Resume</a>
             </div>
         </header>
         <!-- Services-->
